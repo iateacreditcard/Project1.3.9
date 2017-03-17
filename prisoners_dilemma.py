5004414de26ba335c5989c87d2c0ccbfcdc34b59
iateacreditcard <1070241@lammersvilleusd.net>
2017-03-17 14:22:08 -0700

    player 7 change

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -278,16 +278,16 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 7:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'Demon Avenger Powerful Doggo Destroyer of Worlds'
         else:
-            # use history, opponent_history, score, opponent_score
+             # use history, opponent_history, score, opponent_score
             # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
-                return 'c'
+            if len(opponent_history)==0: #It's the first round: betray
+                return 'b'
             elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were sucker last time
+                return 'b' # betray is they were severely punished last time
             else:
-                return 'c' #otherwise collude
+                return 'b' #otherwise collude
 
 
 
